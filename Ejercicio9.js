6154095a0d2450f1a3f72a4eb3c3eb6a3c22e24a
CarlosW117 <90161187+CarlosW117@users.noreply.github.com>
2021-11-29 03:25:52 -0500

    Add files via upload

---
+++ Ejercicio9.js
@@ -0,0 +1,56 @@
+let arreglo = new Array();
+arreglo[0] = new Object();
+arreglo[0]._id = new Object();
+arreglo[0]._id.$oid = "5968dd23fc13ae04d9000001";
+arreglo[0].product_name = "sildenafil citrate";
+arreglo[0].supplier = "Wisozk Inc";
+arreglo[0].quantity = "261";
+arreglo[0].unit_cost = "$10.47";
+//[1]
+arreglo[1] = new Object();
+arreglo[1]._id = new Object();
+arreglo[1]._id.$oid = "5968dd23fc13ae04d9000002";
+arreglo[1].product_name = "sildenafil citrate";
+arreglo[1].supplier = "Wisozk Inc";
+arreglo[1].quantity = "292";
+arreglo[1].unit_cost = "$8.74";
+//[2]
+arreglo[2] = new Object();
+arreglo[2]._id = new Object();
+arreglo[2]._id.$oid = "5968dd23fc13ae04d9000003";
+arreglo[2].product_name = "Dextromathorphan HBr";
+arreglo[2].supplier = "Schmitt-Weissnat";
+arreglo[2].quantity = "211";
+arreglo[2].unit_cost = "$20.53";
+
+console.log(arreglo);
+
+
+
+
+/*
+[{
+  "_id": {
+    "$oid": "5968dd23fc13ae04d9000001"
+  },
+  "product_name": "sildenafil citrate",
+  "supplier": "Wisozk Inc",
+  "quantity": 261,
+  "unit_cost": "$10.47"
+}, {
+  "_id": {
+    "$oid": "5968dd23fc13ae04d9000002"
+  },
+  "product_name": "Mountain Juniperus ashei",
+  "supplier": "Keebler-Hilpert",
+  "quantity": 292,
+  "unit_cost": "$8.74"
+}, {
+  "_id": {
+    "$oid": "5968dd23fc13ae04d9000003"
+  },
+  "product_name": "Dextromathorphan HBr",
+  "supplier": "Schmitt-Weissnat",
+  "quantity": 211,
+  "unit_cost": "$20.53"
+}]*/
